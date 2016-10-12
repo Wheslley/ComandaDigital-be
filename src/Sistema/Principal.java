427f47b86a7e713295a8d1e22b41e789f34cf228
Wheslley <wheslleyny@gmail.com>
2016-10-12 10:16:38 -0300

    Correção da classe Principal

--- src/Sistema/Principal.java
+++ src/Sistema/Principal.java
@@ -1,13 +1,10 @@
 package Sistema;
 
-import Sistema.menus.Menu;
-
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int opcaoMenu = Menu.getMenuPrincipal();
 		
 	}
 
